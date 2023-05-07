47c2ddc8a006e511703354d6f879e0c8329695c1
Serg Yegorov <sergyegorov2012@gmail.com>
2023-05-07 14:59:43 +0300

    Just commit 4a

--- src/com/tl/osv/Log.java
+++ src/com/tl/osv/Log.java
@@ -2,13 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-<<<<<<< HEAD
  * 
  * Test changes 1
  * Test Brunch 2
  * Test changes for brunch 1
-=======
->>>>>>> parent of d6a18fb (Just for fun! :))))
  */
 package com.tl.osv;
 
